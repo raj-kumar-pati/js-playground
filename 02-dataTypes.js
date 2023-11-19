31d4a681aa9883e6c28b219d28636fb1457e7541
raj-kumar-pati <rajkumarpati6969@gmail.com>
2023-11-19 15:10:55 +0530

    my first commit

---
+++ 02-dataTypes.js
@@ -0,0 +1,10 @@
+//there are only 7 data-types.=======>>>>>>>
+
+//number =>till (2)**53
+//bigint =>only for sytific calculation
+//string =>""
+//boolean => true/false
+//null => standalon value
+//undefined => value is not set now.
+//symbol =>unique
+
