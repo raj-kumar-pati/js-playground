936e70a1c03f11f7980e813d6040ae4722d4def1
raj-kumar-pati <rajkumarpati6969@gmail.com>
2023-11-19 15:01:34 +0530

    first commit on 01-variable.js

---
+++ 01-variable.js
@@ -0,0 +1,15 @@
+//there are :=>let ,const ,var(don't use var);
+"use strict";//this will treat js as newer version(es6)
+const name ="raj";
+let score = 15;
+const myscore = [15,16,19,20,23];
+const subjects = {              
+    fist:"A",
+    excilent:"A+",
+    brilient:"AA",
+    god:"B+",
+    notBad:"B"
+}
+console.table([name,score]);
+console.log(myscore);
+console.log(subjects);
